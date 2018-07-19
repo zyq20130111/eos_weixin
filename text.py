eed65fe1f85a22f86f532578b41785ca44377ea2
Ubuntu <ubuntu@ip-172-31-6-14.ap-northeast-1.compute.internal>
2018-07-19 09:15:39 +0000

    dd

---
+++ text.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+
+class Text(object): 
+
+   TEXT1  =  "one weixin can't have more eos account"
+   TEXT2  = "one eos account can't have more weixin"
+   TEXT3 =  "init accounts failed"
+   TEXT4 = "add account failed"
+   TEXT5 = "add account success"
+   TEXT6 = "del acccount"
+   TEXT7 = "delacccount fail"
+   TEXT8 = "AddAccount"
+   TEXT9 = "initAccounts"
+   TEXT10 = "getBlockInfo %d"
+   TEXT11 = "getBlockInfo error"
+   TEXT12 = "getAccount"
+   TEXT13 = "getAccount error "
+   TEXT14 = "sendMsg"
+   TEXT15 = "sendMsg error"
+   TEXT16 = "sendTransertMsg"
+   TEXT17 = "sendTransertMsg error"
+   TEXT18 = "sendVoteMsg"
+   TEXT19 = "sendVoteMsg error"
+   TEXT20 = "getInfo"
+   TEXT21 = "getInfo error"
+   TEXT22 = "没有相应的命令"
+   TEXT23 = 'EOS账号不存在'
+   TEXT24 = "EOS账户绑定成功!"
+   TEXT25 = "0.0000 EOS"
+   TEXT26 = "欢迎使用EOS监控系统！注册EOS账户请使用bind accountname"
+   TEXT27 = "parseBlock"
+   TEXT28 = "parseTransaction"
+   TEXT29 = "parseAction"
+
+
+
+
+
+
+
+
+
+
+
+
