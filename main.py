e3a8efa31662fd8efd96a157178ffe029647a487
Ubuntu <ubuntu@ip-172-31-6-14.ap-northeast-1.compute.internal>
2018-07-25 11:04:31 +0000

    ssssss

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def createMenu():
                     },
                     {
                         "type": "click",
-                        "name": "绑定EOS账号",
+                        "name": "解绑EOS账号",
                         "key": "unbind"
                     },                    
                     {
