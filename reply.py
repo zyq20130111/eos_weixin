2a94e477d7889186cef22c7e34f41685e65fd934
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-07-31 12:52:25 +0800

    remind

--- reply.py
+++ reply.py
@@ -43,6 +43,7 @@ class TextMsg(Msg):
         elif ((opts[0].lower()  == "unbind") and (len(opts) >=2) ):
            return self.unbindEosAccount(opts[1])
         elif((opts[0].lower() == "set") and (len(opts) > 2)):
+           print 'set'
            return self.setEos(opts[1],opts[2])
         elif (opts[0].lower() == "help"):
             return self.helpCmd()
