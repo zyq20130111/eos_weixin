14f6953c3936f7061ac548ec6cffedcfc7aa1c5a
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-08 15:31:34 +0800

    test

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-class Config(object):
+ass Config(object):
    #HTTP_URL  = "http://127.0.0.1:8888/v1/chain/"
    HTTP_URL  = "http://127.0.0.1:8888/v1/chain/"
    DB_SERVER = "localhost"
    DB_USER = "root"
-   DB_PWD  = "galaxy123456@"
-   DB_NAME = "eosdb" 
-   APP_ID  = "wx71b99bd9065e5981"
-   APP_SECRET = "c3557548dbb6f35fea56e3c2453b35ab"
+   DB_PWD  = "123"
+   DB_NAME = "eosdb"
+   APP_ID  = "wx121e16e32b9d1376"
+   APP_SECRET = "5d7faa840d5b702c40cbee4ffb451eb6"
    APP_TOKEN = "galaxy"
    EOSCOUNTINWEIXIN = 2
    WEIXINCOUNTINEOS = 2
-   VOTETEMPLATEID = "3-GYEz_YIaXgp4UXzaqOJoQJ9rKCkiL5Evv6EYmiY0U"
-   TRANSFERTEMPLATEID = "JsWHhFnhsyYTX-25CxpEis4JYpvA4JJj8bB6U4aECoY"
-   START_BLOCK_NUM_ID = 9224915 
+   VOTETEMPLATEID = "3iefblmi4i81uA0FJTsVBtbai0J2vwTDXxhRH7MJqxI"
+   TRANSFERTEMPLATEID = "t_J1Yvjo30U1OPrxt347X4tNZC2TFGN0efAFMrLf53g"
+   START_BLOCK_NUM_ID = 1420559
    MEDIAID = "YuMg5nqDjPyFQH24nUbD24kkhQMd_OEv5b2YfddpKLU"
