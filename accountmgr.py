d7d4f9501d13c86fea4b1cb641cd9a6079ea6b4f
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-08-10 16:53:45 +0800

    log

--- accountmgr.py
+++ accountmgr.py
@@ -315,7 +315,7 @@ class AccountMgr(object):
 
     def addTransfer(self,transferor,recipient,date,quantity):
 
-       
+       print "addTransfer"       
        Logger().Log("addTransfer")
        try:
            db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )           
