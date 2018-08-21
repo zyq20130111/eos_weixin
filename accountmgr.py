f972275d10c876e91ebea05140ebe9c934aad4f2
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-08-21 10:45:04 +0800

    vote

--- accountmgr.py
+++ accountmgr.py
@@ -287,7 +287,8 @@ class AccountMgr(object):
 
 
     def addVote(self,vote,pb,votenum,date):
-     
+       
+       return 
        print("addVote") 
        try:
            db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
@@ -316,7 +317,7 @@ class AccountMgr(object):
 
     def addTransfer(self,transferor,recipient,date,quantity):
 
-         
+       return  
        print("addTransfer")
        try:
            db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
