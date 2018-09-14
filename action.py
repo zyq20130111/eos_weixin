9bad52ea01dae416f20a960369cb3323eb5e4fb0
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-09-14 15:27:46 +0800

    vote

--- action.py
+++ action.py
@@ -20,10 +20,10 @@ class Action(object):
             db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
             cursor = db.cursor()
 
-            openid = data.openid
-            user = data.user
+            #openid = data.openid
+            #user = data.user
             
-            return openid
+            #return openid
            
             #sql = "SELECT * FROM order_tbl where open_id ='%s' and username = '%s'" %(openid,account)
             #cursor.execute(sql)
