564edc99388bb86444b9b6c497279f2547bc8b7e
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-09-14 14:38:46 +0800

    vote

--- action.py
+++ action.py
@@ -21,6 +21,7 @@ class Action(object):
 
             openid  = data.openid
             account = data.account
+            return openid
            
             sql = "SELECT * FROM order_tbl where open_id ='%s' and username = '%s'" %(openid,account)
             cursor.execute(sql)
