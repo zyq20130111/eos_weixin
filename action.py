733435d57589bb0976b4d50dd670e140d9624deb
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-09-14 15:42:04 +0800

    vote

--- action.py
+++ action.py
@@ -17,16 +17,13 @@ class Action(object):
             if len(data) == 0:
                 return "hello, this is handle view"
 
+            openid = data.openid
+            user = data.user
+
             db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
-
             cursor = db.cursor()
-            print data.openid
-            print data.user
-
-            #openid = data.openid
-            #user = data.user
-            
-            #return openid
+ 
+            return openid
            
             #sql = "SELECT * FROM order_tbl where open_id ='%s' and username = '%s'" %(openid,account)
             #cursor.execute(sql)
