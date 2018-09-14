9a7699388627214a332063b332c5ac7f61ee34a3
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-09-14 15:01:38 +0800

    vote

--- action.py
+++ action.py
@@ -11,7 +11,7 @@ from config import Config
 
 class Action(object):
 
-    def GET(self):
+    def GET(self,openid,account):
         try:
             data = web.input()
             if len(data) == 0:
@@ -20,9 +20,7 @@ class Action(object):
             db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
             cursor = db.cursor()
 
-            openid  = data.openid
-            acc = data.acc
-            return acc
+            return account
            
             #sql = "SELECT * FROM order_tbl where open_id ='%s' and username = '%s'" %(openid,account)
             #cursor.execute(sql)
