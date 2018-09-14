05c61323df476d8bb8ebd91a67a191d6f2ea9080
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-09-14 14:50:16 +0800

    vote

--- action.py
+++ action.py
@@ -17,8 +17,8 @@ class Action(object):
             if len(data) == 0:
                 return "hello, this is handle view"
 
-            #db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
-            #cursor = db.cursor()
+            db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
+            cursor = db.cursor()
 
             #openid  = data.openid
             #account = data.account
