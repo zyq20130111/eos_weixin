bb04f03d7f1906bc180f3d0b1800e199c3810977
System Administrator <root@zhangfanfandeMacBook-Pro.local>
2018-09-14 16:39:44 +0800

    vote

--- action.py
+++ action.py
@@ -18,11 +18,11 @@ class Action(object):
             if len(data) == 0:
                 return "hello, this is handle view"
 
-            jsdata  = json.loads(data)
-            return jsdata            
-            openid = jsdata['openid']
-            account = jsdata['account']
-
+            #jsdata  = json.loads(data)
+            #return jsdata            
+            openid = data['openid']
+            account = data['account']
+            return openid
             return account
             db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
             cursor = db.cursor()
