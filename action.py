b794e1d31e75699c2eacf1902227f9b52242a28a
章凡凡 <zhangfanfan@zhangfanfandeMacBook-Pro.local>
2018-09-14 14:43:41 +0800

    vote

--- action.py
+++ action.py
@@ -16,19 +16,22 @@ class Action(object):
             if len(data) == 0:
                 return "hello, this is handle view"
 
-            db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
-            cursor = db.cursor()
+            #db = MySQLdb.connect(Config.DB_SERVER, Config.DB_USER, Config.DB_PWD, Config.DB_NAME, charset='utf8' )
+            #cursor = db.cursor()
 
             openid  = data.openid
             account = data.account
             return openid
            
-            sql = "SELECT * FROM order_tbl where open_id ='%s' and username = '%s'" %(openid,account)
-            cursor.execute(sql)
+            #sql = "SELECT * FROM order_tbl where open_id ='%s' and username = '%s'" %(openid,account)
+            #cursor.execute(sql)
 
-            cursor.fetchall()
-            if(cursor.rowcount <= 0):
-               AccountMgr().Instance().AddAccount(openid,account,"demo")                    
+            #cursor.fetchall()
+            #if(cursor.rowcount <= 0):
+               #AccountMgr().Instance().AddAccount(openid,account,"demo")                    
+
+            #cursor.close()
+            #db.close()
 
             return '{"code":0}' 
 
